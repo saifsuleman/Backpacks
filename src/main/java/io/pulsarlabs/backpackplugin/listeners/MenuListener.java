59ebd402e66c872bd9645a4c4d88dedef7032aed
Saif <50113408+saifsuleman@users.noreply.github.com>
2022-12-09 19:59:59 +0000

    aa

--- src/main/java/io/pulsarlabs/backpackplugin/listeners/MenuListener.java
+++ src/main/java/io/pulsarlabs/backpackplugin/listeners/MenuListener.java
@@ -1,12 +1,14 @@
 package io.pulsarlabs.backpackplugin.listeners;
 
 import io.pulsarlabs.backpackplugin.BackpackPlugin;
+import io.pulsarlabs.backpackplugin.menu.MenuBuilder;
 import io.pulsarlabs.backpackplugin.menu.MenuHolder;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 
 public class MenuListener implements Listener {
     public MenuListener() {
@@ -17,9 +19,14 @@ public class MenuListener implements Listener {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
         Inventory inventory = event.getClickedInventory();
-        if (!(inventory instanceof MenuHolder)) {
-
+        if (inventory == null) {
+            return;
         }
+        if (!(inventory.getHolder() instanceof MenuHolder)) {
+            return;
+        }
+        MenuHolder holder = (MenuHolder) inventory.getHolder();
+        MenuBuilder builder = holder.getBuilder();
     }
 
     @EventHandler
